4e88f3a18c1bcfefbf5649243978c126b4445d9a
tdavidsonas88 <32385991+tdavidsonas88@users.noreply.github.com>
2017-12-06 13:10:57 +0200

    Hello Toast app

---
+++ app/src/main/java/lt/tadasdavidsonas88/hellotoast/MainActivity.java
@@ -0,0 +1,41 @@
+package lt.tadasdavidsonas88.hellotoast;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    /**
+     * When the TOAST button is clicked, show a toast.
+     * @param view - view the view that triggers  this onClick handler.
+     *             Since a toast always show on the top, view is not used.
+     */
+    public void showToast(View view) {
+        // Create a toast
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_LONG);
+        // Show toast
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if(mShowCount != null){
+            mShowCount.setText(Integer.toString(mCount));
+        }
+    }
+}
